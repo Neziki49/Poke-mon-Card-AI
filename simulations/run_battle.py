668551439c27897663c51b4f8182c9958b724ff8
Neziki49 <ujikikun0414@gmail.com>
2026-08-10 20:44:41 +0900

    更新

---
+++ simulations/run_battle.py
@@ -0,0 +1,36 @@
+import json
+import random
+
+from cg.game import battle_start, battle_finish, battle_select, visualize_data
+
+def random_agent(obs_dict: dict) -> list[int]:
+    return random.sample(list(range(len(obs_dict["select"]["option"]))), obs_dict["select"]["maxCount"])
+
+def load_deck(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return [int(line.strip()) for line in f if line.strip()]
+    
+deck1 = load_deck("../decks/Dragapult/deck.csv")
+deck2 = load_deck("../decks/Dragapult/deck.csv")
+
+obs_dict, _ = battle_start(deck1, deck2)
+obs_log = [""]
+action_log = [None]
+print(json.dumps(obs_dict, indent=2, ensure_ascii=False))
+while True:
+    if obs_dict["current"]["result"] >= 0:
+        break
+    action = random_agent(obs_dict)
+    obs_dict.pop("search_begin_input")
+    obs_log.append(obs_dict)
+    action_log.append(action)
+    obs_dict = battle_select(action)
+
+vis = json.loads(visualize_data())
+for i in range(len(vis)):
+    vis[i]["obs"] = obs_log[i]
+    vis[i]["action"] = [action_log[i], action_log[i]]
+with open("vis.json", "w") as file:
+    json.dump(vis, file)
+
+battle_finish()
